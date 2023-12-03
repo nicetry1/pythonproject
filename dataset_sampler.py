bcee498ed19a9fa332ebe6ef314128172f1d7414
nicetry1 <83890805+nicetry1@users.noreply.github.com>
2023-12-03 15:41:13 -0500

    fixed error in int notation

--- dataset_sampler.py
+++ dataset_sampler.py
@@ -7,12 +7,15 @@ def generate_random_sample(input_file, output_file, sample_size):
         all_lines = [json.loads(line) for line in input_stream]
 
         #sample lines
-        random_sample = random.sample(all_lines, min(sample_size), len(all_lines))
+        random_sample = random.sample(all_lines, min(sample_size, len(all_lines)))
 
         with open(output_file, 'w') as output_stream:
-            for item in random_sample:
+            for i, item in enumerate(random_sample, start=1):
                 #write sample to output file
                 output_stream.write(json.dumps(item, ensure_ascii=False) + '\n')
+                #print progress message every 100 lines processed
+                if i % 100 == 0:
+                    print(f'processed {i} lines out of {len(random_sample)}')
 
 if __name__ == '__main__':
     #define filepaths and sample size
